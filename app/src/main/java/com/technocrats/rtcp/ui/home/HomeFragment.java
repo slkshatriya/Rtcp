3dacc5eba10213f3e4818777f4dd77906640d0dd
surajsingh <surajkeshr023@gmail.com>
2020-03-01 12:57:08 +0530

    after merge

--- app/src/main/java/com/technocrats/rtcp/ui/home/HomeFragment.java
+++ app/src/main/java/com/technocrats/rtcp/ui/home/HomeFragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
+import com.technocrats.rtcp.Guide;
 import com.technocrats.rtcp.R;
 import com.technocrats.rtcp.basicsscreen;
 import com.technocrats.rtcp.ui.slideshow.SlideshowFragment;
@@ -23,7 +24,7 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     Button basicsofc, compitetiveofc;
-    CardView card,card1,card3;
+    CardView card,card1,card2,card3;
     View root;
     CarouselView carouselView;
     int[] sampleImages = {R.drawable.comp1, R.drawable.comp2, R.drawable.comp, R.drawable.comp4};
@@ -34,11 +35,18 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
           root = inflater.inflate(R.layout.fragment_home, container, false);
         card = root.findViewById(R.id.card);
+        card2 = root.findViewById(R.id.card2);
         carouselView = root.findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
         card1 = root.findViewById(R.id.card1);
         card3 = root.findViewById(R.id.card3);
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity6();
+            }
+        });
         card3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,6 +81,10 @@ public class HomeFragment extends Fragment {
     public void openActivity4(){
         Intent home1 = new Intent(getContext(), SlideshowFragment.class);
         startActivity(home1);
+    }
+    public void openActivity6(){
+        Intent guide = new Intent(getContext(), Guide.class);
+        startActivity(guide);
     }
     ImageListener imageListener = new ImageListener() {
         @Override
